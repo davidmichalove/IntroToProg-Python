3d65a212dbe888ea82668f649f4896aeae39f5fd
davidmichalove <133909395+davidmichalove@users.noreply.github.com>
2023-05-18 08:57:12 +0200

    Add files via upload
    My apologies, I realized, only now, that I had uploaded the wrong .py file.

--- Assigment05_Starter.py
+++ Assigment05_Starter.py
@@ -1,157 +1,111 @@
-# ------------------------------------------------- #
-# Title: <Type the name of the script here>
-# Description: <Type a description of the script>
-# ChangeLog: (Who, When, What)
-# <Example Dev,01/01/2030,Created Script>
-# ------------------------------------------------- #
+# ------------------------------------------------------------------------ #
+# Title: Assignment 05
+# Description: Working with Dictionaries and Files
+#              When the program starts, load each "row" of data
+#              in "ToDoList.txt" into a python Dictionary.
+#              Add each dictionary "row" to a python list "table"
+# ChangeLog (Who,When,What):
+# RRoot,1.1.2030,Created started script
+# <David Michalove>,<05/15/23>,Added code to complete assignment 5
+# ------------------------------------------------------------------------ #
 
 # -- Data -- #
+# declare variables and constants
+objFile = "ToDoList.txt"   # An object that represents a file
+strData = ""  # A row of text data from the file
+dicRow = {}    # A row of data separated into elements of a dictionary {Task,Priority}
+lstTable = []  # A list that acts as a 'table' of rows
+strMenu = ""   # A menu of user options
+strChoice = "" # A Capture the user option selection
+strFile = None # File handle
+
 # -- Processing -- #
-# -- Presentation (I/O) -- #
-
-objFile = open("HomeInventory.txt", "a")
-lstItems = []
-lstCosts = []
-lstTable = [lstItems, lstCosts]
-def Menu():
-# This function allows the menu to be used various times without unnecessary lines of code.
-    print("Menu of Options")
-    print("1) Add Data to List")
-    print("2) Display Current Data")
-    print("3) Exit and Save to File")
-Menu()
-answer = input("Which option would you like to perform? [1 to 3] - ")
-# This input response allows the while loop to begin.
-while answer != "1" and answer != "2" and answer != "3":
-# Uses the negation cases because this part tests for prohibited user inputs.
-    print("Please only respond with: (1, 2, or 3) ")
-    Menu()
-    answer = input("Which option would you like to perform? [1 to 3] - ")
-    # This ensures that the program will not accept any other response than the 3 integers.
-while answer == "1" or answer == "2":
-# Loops through until user enters "3" after each section.
-    if answer == "1":
-    # If user enters "1" then user can enter primary data.
-        item = input("Name of item: ")
-        cost = input("Value of " + item + ": ")
-        lstItems.append(item)
-        lstCosts.append(cost)
-        Menu()
-        answer = input("Which option would you like to perform? [1 to 3] - ")
-        # This allows user to break this section of the while loop
-        # and enter into the "elif answer == "2"" section or end the program.
-    elif answer == "2":
-        for i in range(len(lstItems)):
-            print(lstItems[i]+ ":\t\t$" + lstCosts[i])
-        Menu()
-        answer = input("Which option would you like to perform? [1 to 3] - ")
-        # Allows the user to enter into the while loop again or leave the program.
-
-for i in range(len(lstItems)):
-    save = (lstItems[i] + ":\t\t$" + lstCosts[i] +"\n")
-    # Saves each item with cost to a text file.
-    objFile.write(save)
-print("Finished")
-input("enter 'exit' to leave")
-
-
-# strFile = open(objFile, "w")
+# Step 1 - When the program starts, load any data you have
+# in a text file called ToDoList.txt into a python list of dictionaries rows (like Lab 5-2)
+print("\n Here are your current items if any:")
 strFile = open(objFile, "r")
+# Reads the data in the txt file
 for row in strFile:
+# Loops through the content within the txt file
     lstRow = row.split(",")
-    # print(lstRow)
-    # print(len(lstRow))
-    # print(lstRow[0] + " | " + lstRow[1].strip())
+    # splits apart data based on a comma and returns a list
     dicRow = {"task":lstRow[0],"priority":lstRow[1].strip()}
+    lstTable.append(dicRow)
     print(dicRow)
-strFile.close()    # diction = {lstRow[0], lstRow[1].strip()}
-    # print(diction)
+strFile.close()
 
+# -- Input/Output -- #
+# Step 2 - Display a menu of choices to the user
+while (True):
+    print("""
+    Menu of Options
+    1) Show current data
+    2) Add a new item.
+    3) Remove an existing item.
+    4) Save Data to File
+    5) Exit Program
+    """)
+    strChoice = str(input("Which option would you like to perform? [1 to 5] - "))
+    print()  # Adding a new line for looks
+    # Step 3 - Show the current items in the table
+    if (strChoice.strip() == '1'):
+        for objRow in lstTable:
+        # Prints the contents of lstTable
+            print(objRow)
+        continue
+    # Step 4 - Add a new item to the list/Table
+    elif (strChoice.strip() == '2'):
+        strTask = input("Enter Task: ")
+        # Saves the user's input to the key: task.
+        strPriority = input("Enter Priority: ")
+        # Saves the user's input to the key: priority.
+        dicRow = {"task": strTask, "priority": strPriority}
+        # Main dictionary that will make up the lstTable
+        lstTable.append(dicRow)
+        # Puts dictionary into a list making a table
+        continue
+    # Step 5 - Remove a new item from the list/Table
+    elif (strChoice.strip() == '3'):
+        amountOptions = len(lstTable)
+        # Gets the length of the table for indexing
+        print("Here are your tasks so far:")
+        for objRow in lstTable:
+        # Prints the contents of lstTable
+            print(objRow)
 
-# TODO: Add Code Here
-for objRow in lstTable:
-    print(objRow)
-# for myKey, myValue in dicRow.items():
-#     print(myKey, " = ", myValue)
-# strFile = open(objFile, "r")
-# for row in strFile:
-#     lstRow = row.split(",")
-#     # print(lstRow[0] + " | " + lstRow[1].strip())
-#     dicRow = {"task": lstRow[0], "priority": lstRow[1].strip()}
-#     print(
-#     print(dicRow)
-# strFile.close()
-
-  # print(lstTable.get("task") + ", " + lstTable.get("priority"))
-
-        # print(dItem.get("task"))
-        # print(dItem.get("priority"))
-        # for task, priority in dItem.items():
-        #     print(task, priority)
-        # for objRow in lstTable:
-        #     print(objRow)
-            # print(frsRow[0] + ", " + frsRow[1].strip())
-
-        # strFile.write(lstTable)
-        # for item in lstTable:
-        #     print(item)
-            # dicRow = {"task": lstRow[0], "priority": lstRow[1].strip()}
-            # print(dicRow)
-            # for myKey, myValue in dicRow.items():
-            #     print(myKey, ", ", myValue)
-            # objFile.write(myKey, ", ", myValue)
-        # for task, priority in dicRow.items():
-        #     print(task, ", ", priority)
-        # for row in lstTable:
-        #     print(task, ", ", priority)
-        # for task, priority in lstTable.items():
-        #     print(task, ", ", priority)
-        # objFile.write(myKey, ", ", myValue)
-        # strFile.close()
-
-        # for i in range(len(lstItems)):
-        #     save = (lstItems[i] + ":\t\t$" + lstCosts[i] + "\n")
-        #     # Saves each item with cost to a text file.
-        #     objFile.write(save)
-
-
-# for i in range(len(lstTable)):
-#     # print(lstTable[i])
-#     dItem = lstTable[i]
-#     print(dItem.get("task") + ", " + dItem.get("priority"))
-#     objFile.write(dItem.get("task") + ',' + dItem.get("priority") + "\n")
-
-
-# while newTable.index(dicRow):
-#     toDel = newTable.index(dicRow)
-#     del newTable[toDel]
-
-# if newTable.index(dicRow):
-#     toDel = newTable.index(dicRow)
-#     del newTable[toDel]
-
- # strFile = open(objFile, "w")
-        # objFile = open("ToDoList.txt", "a")
-        # print(lstTable[0])
-        # dItem = lstTable[0]
-        # print(dItem.get("task") + ", " + dItem.get("priority"))
-
-
-# for i in range(len(lstTable)):
-#     # print(lstTable[i])
-#     dItem = lstTable[i]
-#     print(dItem.get("task") + ", " + dItem.get("priority"))
-#     objFile.write(dItem.get("task") + ',' + dItem.get("priority") + "\n")
-
+        term = int(input("What task/priority do you wish to remove?\nPlease choose from 0 to "+ str(amountOptions - 1) + ": "))
+        # Gets the user's input for what term is wished to be removed
+        while term >= amountOptions:
+            # Uses the greater than or equal to cases because this part tests for an out of range index selection
+            print("Please only respond with a number from 0 to " + str(amountOptions - 1) +".")
+            term = int(input("What task/priority do you wish to remove?\nPlease choose from 0 to " + str(amountOptions - 1) + ": "))
+        yesNo = input("You wish to remove: " + str(lstTable[term]) + "? [yes/no] ")
+        # Asks user if they wish to proceed
+        print(lstTable[term])
+        if yesNo.lower() == "yes":
+            lstTable.pop(term)
+            # Removes and returns the selected dictionary so the user can see what has been deleted
+        continue
     elif (strChoice.strip() == '4'):
-        # TODO: Add Code Here
-        # newTable = lstTable.copy()
-
-        # strFile = open(objFile, "r")
-        # for row in strFile:
-        #     lstRow = row.split(",")
-        #     dicRow = {"task": lstRow[0], "priority": lstRow[1].strip()}
-        #     newTable.index(dicRow)
-        #     toDel = newTable.index(dicRow)
-        #     del newTable[toDel]
-        # strFile.close()
+    # Step 6 - Save tasks to the ToDoList.txt file
+        open('ToDoList.txt', 'w').close()
+        # Clears what is in the txt file so nothing will be duplicated by writing lstTable into the txt file
+        objFile = open("ToDoList.txt", "a")
+        for i in range(len(lstTable)):
+        # Saves each dictionary pair to a txt file
+            dItem = lstTable[i]
+            print(dItem.get("task") + ", " + dItem.get("priority"))
+            objFile.write(dItem.get("task") + ', ' + dItem.get("priority") + "\n")
+        objFile.close()
+        print("Saved!")
+        continue
+    # Step 7 - Exit program
+    elif (strChoice.strip() == '5'):
+        finalYesNo = input("Do you wish to end the program? [yes/no] ")
+        while finalYesNo.lower() != "yes" and finalYesNo.lower() != "no":
+        # Ensures user will only respond within the asked for parameters
+            print("Please only respond with: [yes/no] ")
+            finalYesNo = input("Do you wish to end the program? [yes/no] ")
+        if finalYesNo.lower() == "no":
+            continue
+        break # and Exit the program
